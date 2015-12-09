262de41cce71902fd64e45c614c8b2e254d9aae9
Jerrod Crook <jerrodcrook@Jerrods-MBP.lan>
2015-12-08 22:22:16 -0700

    added auth

--- config/app.php
+++ config/app.php
@@ -137,6 +137,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Bestmomo\Scafold\ScafoldServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
